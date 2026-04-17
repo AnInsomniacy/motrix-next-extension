5d1cf37f1a561cd30fb088c792469c7b822f1f74
sekiro <qq1326555262@gmail.com>
2026-04-17 10:37:58 +1000

    feat: add data_collection_permissions to gecko manifest options in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -42,6 +42,9 @@ export default defineConfig({
         gecko: {
           id: 'motrix-next-extension@aninsomniacy.dev',
           strict_min_version: '128.0',
+          data_collection_permissions: {
+            required: ['none'],
+          },
         },
       },
     }),
